6d193a63e129de8fb0986775ac9e67624101bcec
sauravh <mail@sauravh.com>
2020-03-12 21:01:50 +0530

    Update index.js

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="footer">
-    &copy; 2019 Ujjivan Small Finance Bank Ltd. CIN NO: L65110DL2016PLC302481
+    &copy; 2019 UME BANK Finance Bank Ltd. 
     </div>
   );
 }
